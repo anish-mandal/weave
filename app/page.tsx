65c97744cb5f221248050a86ca71243f591e1751
Anish Mandal <anishmandalme1234@outlook.com>
2025-11-10 23:11:01 +0530

    feat: add signup form and logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
-  return (<div className="m-5">
-    <h1 className="font-bold text-[clamp(3rem,15vw,12rem)] leading-none">weave</h1>
-    <hr/>
-    <p className="font-display text-2xl mt-3">Connect. Create. Collaborate. <Link href="/auth"><button className="border border-b-white px-3 rounded-full cursor-pointer">Sign Up</button></Link>
-</p>
-    </div>);
+  return (
+    <div className="m-5">
+      <div className="flex gap-5">
+        <Image src="/weave-logo.svg" alt="Weave Logo" width={150} height={100} className="" />
+        <h1 className="font-bold text-[clamp(3rem,15vw,12rem)] leading-none">weave</h1>
+      </div>
+      <hr/>
+      <p className="font-display text-2xl mt-3">Connect. Create. Collaborate.{" "}
+        <Link href="/auth"><button className="text-2xl">Sign Up</button></Link>
+      </p>
+    </div>
+  );
 }
